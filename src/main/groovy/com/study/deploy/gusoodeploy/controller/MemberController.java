987179eded4a1474db9e8d2a14bd3c8c6d011be3
Dom1046 <rkrkwnj1046@gmail.com>
2024-11-13 01:11:24 +0900

    asdfasdf

--- src/main/groovy/com/study/deploy/gusoodeploy/controller/MemberController.java
+++ src/main/groovy/com/study/deploy/gusoodeploy/controller/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/member/post")
+@RequestMapping("api/v1/member")
 public class MemberController {
 
     private final MemberService memberService;
